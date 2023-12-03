b4bf75d7ebc38e4e5fc35b00358b2899270afe0f
Shaharyar <Shaharyar321321@gmail.com>
2023-12-03 17:22:11 +0500

    initial commit

---
+++ init.lua
@@ -0,0 +1,5 @@
+require("shaharyar")
+require("shaharyar.keymap")
+require("shaharyar.set")
+require("after.plugin.colors")
+
---
+++ lua/shaharyar/packer.lua
@@ -0,0 +1,43 @@
+
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.4',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+	use { "catppuccin/nvim", as = "catppuccin", config = function() vim.cmd('colorscheme catppuccin')  end }
+
+	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+	use('nvim-treesitter/playground')
+	use "nvim-lua/plenary.nvim"
+	use {
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		requires = { {"nvim-lua/plenary.nvim"} }
+	}
+	use('mbbill/undotree')
+	use('tpope/vim-fugitive')
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v3.x',
+		requires = {
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},
+			-- Autocompletion
+			{'hrsh7th/nvim-cmp'},
+			{'hrsh7th/cmp-nvim-lsp'},
+			{'L3MON4D3/LuaSnip'},
+		}
+	}
+
+	use {
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+    }
+   use("folke/neodev.nvim")
+   use("github/copilot.vim")
+end)
